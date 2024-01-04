5e721e460c231b1be8dac6aa82cba91046ea9ea6
Dadflip <dadflipper.1@gmail.com>
2024-01-04 08:49:27 +0100

    UPDATE
    Removing .zip

---
+++ scripts/SCRIPT_addon-adb-for-apk-installation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y adb
+adb connect arc
+echo "TERMINÉ"
---
+++ scripts/SCRIPT_addon-timeshift-system-restauration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+sudo apt install -y timeshift
+echo "install verified" > /etc/timeshift-verif-file
+echo "TERMINÉ"
